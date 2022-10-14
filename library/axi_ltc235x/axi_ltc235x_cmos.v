5ec11c76e814177d7e040ae369c3a6df714bc2f1
Jem Geronimo <Johnerasmusmari.Geronimo@analog.com>
2022-10-14 15:25:17 +0800

    comment out unsupported parameters
    Signed-off-by: Jem Geronimo <Johnerasmusmari.Geronimo@analog.com>

--- library/axi_ltc235x/axi_ltc235x_cmos.v
+++ library/axi_ltc235x/axi_ltc235x_cmos.v
@@ -38,8 +38,8 @@
 module axi_ltc235x_cmos #(
 	parameter NUM_CHANNELS = 8,	// 8 for 2358, 4 for 2357, 2 for 2353
 	parameter DATA_WIDTH = 18,	// 18 or 16
-  parameter ACTIVE_LANE = 8'b1111_1111,
-	parameter SOFTSPAN
+  parameter ACTIVE_LANE = 8'b1111_1111
+	//parameter SOFTSPAN
 ) (
 
   input                   rst,
