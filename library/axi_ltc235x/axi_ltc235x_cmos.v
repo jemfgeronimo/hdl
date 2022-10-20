371b7f362955dec264728e289f41234b127ae879
Jem Geronimo <Johnerasmusmari.Geronimo@analog.com>
2022-10-20 11:46:01 +0800

    add note for adc_data formatting
    Signed-off-by: Jem Geronimo <Johnerasmusmari.Geronimo@analog.com>

--- library/axi_ltc235x/axi_ltc235x_cmos.v
+++ library/axi_ltc235x/axi_ltc235x_cmos.v
@@ -428,7 +428,7 @@ module axi_ltc235x_cmos #(
   genvar i;
   generate
     for (i=0; i < 8; i=i+1) begin: format
-      assign adc_data_s[i] = {{14{0}}, adc_data_store[i][23:6]};
+      assign adc_data_s[i] = {{14{0}}, adc_data_store[i][23:6]};  // TODO: find out if data is needed to be formatted or not yet
       assign adc_ch_id_s[i] = adc_data_store[i][5:3];
       assign adc_softspan_s[i] = adc_data_store[i][2:0];
     end
