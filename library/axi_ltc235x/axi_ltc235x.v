bdebb13519d84cc6ac94f98c71c8850e445cf242
Jem Geronimo <Johnerasmusmari.Geronimo@analog.com>
2022-10-21 15:56:10 +0800

    axi_ltc235x: add TODO comments for lvds support and other ltc235x support
    Signed-off-by: Jem Geronimo <Johnerasmusmari.Geronimo@analog.com>

--- library/axi_ltc235x/axi_ltc235x.v
+++ library/axi_ltc235x/axi_ltc235x.v
@@ -214,7 +214,7 @@ module axi_ltc235x #(
       assign adc_clk_s = up_clk;
     end
 
-    if (LVDS_CMOS_N == 1) begin
+    if (LVDS_CMOS_N == 1) begin // TODO: add support for LVDS interface
       assign scki = 1'b0;
     end else begin
       assign scki_p = 1'b0;
@@ -418,5 +418,7 @@ module axi_ltc235x #(
     .up_raddr (up_raddr_s),
     .up_rdata (up_rdata),
     .up_rack (up_rack));
+
+  // TODO: add support for other LTC235x members
 
 endmodule
