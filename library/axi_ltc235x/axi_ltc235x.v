903a5b31bd113b1c5f976c19b2e549fafd140ed3
Jem Geronimo <Johnerasmusmari.Geronimo@analog.com>
2022-11-09 14:58:07 +0800

    comment: internal register for softspan_next config
    Signed-off-by: Jem Geronimo <Johnerasmusmari.Geronimo@analog.com>

--- library/axi_ltc235x/axi_ltc235x.v
+++ library/axi_ltc235x/axi_ltc235x.v
@@ -53,7 +53,7 @@ module axi_ltc235x #(
   parameter       LANE_7_ENABLE = "true",
   parameter       NUM_CHANNELS = 8,	// 8 for 2358, 4 for 2357, 2 for 2353
   parameter       DATA_WIDTH = 18,	// 18 or 16
-  parameter       SOFTSPAN_NEXT = 24'hff_ffff, // TODO: make this an input signal
+  parameter       SOFTSPAN_NEXT = 24'hff_ffff, // TODO: internal register for this
   parameter       EXTERNAL_CLK = 0
 ) (
 
