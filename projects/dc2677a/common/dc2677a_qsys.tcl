37f59c73bf14f5b1f7c6fee2b756a2367ef22c47
Jem Geronimo <Johnerasmusmari.Geronimo@analog.com>
2022-11-09 13:28:46 +0800

    add: common files of project dc2677a
    Signed-off-by: Jem Geronimo <Johnerasmusmari.Geronimo@analog.com>

---
+++ projects/dc2677a/common/dc2677a_qsys.tcl
@@ -0,0 +1,17 @@
+# axi_ltc235x
+
+# pwm gen
+
+# cpack
+
+# dmac
+
+# interrupts / cpu interrupts
+
+# cpu interconnects / address map
+
+# mem interconnects / dma interconnects
+
+# gpio ?
+
+# clocks and resets ?
